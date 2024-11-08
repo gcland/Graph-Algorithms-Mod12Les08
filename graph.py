b1feb11e3a43b4cf670d383f3f02fd9fd633ba92
gcland <grantbcopeland@gmail.com>
2024-11-08 12:52:56 -0600

    Added time / space complexity analysis, added refinement to routes
    Added time / space complexity analysis
    Added refinement to routes:
    - routes now works more consistently with longer routes. Routes of steps > 3 still tends to yield bugs still. Further refinement/quality check needed.

--- graph.py
+++ graph.py
@@ -1,3 +1,6 @@
+# *** Routes algorithm not fully correct yet; tends to yield errors on routes with > 3 steps. ***
+# ** Needs recursive functions/quality check to weed out bugs. Will implemented at a later date. ** 
+
 import heapq
 
 class Graph:
@@ -26,26 +29,28 @@ class Graph:
         pq = [(0,start)]
 
         while pq:
-            route = []
             current_distance, current_vertex = heapq.heappop(pq)
             if current_distance > distances[current_vertex]:
                 continue
-            for neighbor, weight in self.vertices[current_vertex].items():       
+            for neighbor, weight in self.vertices[current_vertex].items():  
+                route = []    
                 distance = current_distance + weight
                 if distance < distances[neighbor]:
-                    step = {current_vertex: current_distance}
-                    if start not in step:
-                        route.append(step)
-                    step = {neighbor: weight}
-                    if start not in step:
-                        route.append(step)
+                    if start != current_vertex:
+                        step = {current_vertex: current_distance}
+                        if start not in step:
+                            route.append(step)
+                        step = {neighbor: weight}
+                        if start not in step:
+                            route.append(step)
+                    else:
+                        step = {neighbor: weight}
+                        if start not in step:
+                            route.append(step)
                     paths[neighbor] = route
                     distances[neighbor] = distance
                     heapq.heappush(pq, (distance, neighbor))
-            
-        for item in paths:
-            if start in self.vertices[item]:
-                paths[item] = [{item: self.vertices[item][start]}]
+        paths = dict(sorted(paths.items()))
         return distances, paths
 
 graph = Graph()
@@ -55,32 +60,73 @@ graph.add_vertex('B')
 graph.add_vertex('C')
 graph.add_vertex('D')
 graph.add_vertex('E')
+graph.add_vertex('F')
+graph.add_vertex('G')
 
 graph.add_edge('A', 'B', 7)
 graph.add_edge('B', 'C', 2)
 graph.add_edge('A', 'C', 8)
-graph.add_edge('A', 'D', 12)
+graph.add_edge('F', 'D', 12)
 graph.add_edge('B', 'E', 10)
 graph.add_edge('D', 'E', 4)
-
-start = 'E'
+graph.add_edge('F', 'B', 2)
+graph.add_edge('G', 'C', 5)
+# Vertices
+# {'A': {'B': 7, 'C': 8}, 
+# 'B': {'A': 7, 'C': 2, 'E': 10, 'F': 2}, 
+# 'C': {'B': 2, 'A': 8, 'G': 5}, 
+# 'D': {'F': 12, 'E': 4}, 
+# 'E': {'B': 10, 'D': 4}, 
+# 'F': {'D': 12, 'B': 2}, 
+# 'G': {'C': 5}}
+start = 'G'
 end = 'A'
+print('Vertices:', graph.vertices)
 distances, paths = graph.dijkstra(start)
 print('\nMinimum distances between points:', distances)
-print(f'Paths between start point {start}: {paths}')
+def pretty_paths(paths):
+    print('\nPaths:')
+    for path in paths:
+        if len(paths[path]) > 1:
+            i = 0
+            for step in paths[path]:
+                if path not in step:
+                    step = paths[list(step.keys())[0]]
+                    paths[path][i] = step
+                    del paths[path][i]
+                    for item in step:
+                        j=0
+                        paths[path].insert(j, item)
+                        j+=1
+                    i+=1
+        print(f'{path} - {paths[path]}')
+    print(paths)
+pretty_paths(paths)
 def display_route(start, end, paths):
     if end not in paths:
         print('\nThe end point is not within the paths database. Please try again!')
     else:
         i=1
-        print(f'\nStart: {start}.')
+        print('\n-*-*-Route-*-*-')
+        print(f'\nStart: {start}. End: {end}')
+        total = 0
         edge0 = start
         for item in paths[end]:
             for point, distance in item.items():
                 edge1 = point
                 print(f'{i}. Take {edge0}{edge1} - {distance} units.')
+                total += distance
                 edge0 = point
                 i+=1
         print(f'{i}. Arrive at {end}.')
+        print(f'Total distance: {total} units.')
         print('Thanks for using Routes!\n')
 display_route(start, end, paths)
+
+
+# Time complexity
+# dijkstra's algorithm is a quadratic time complexity because of the while loop and the for loop within
+
+# Space complexity
+# the version of dijkstra's algorithm has 2 dictionaries and 2 lists running. Lists and dictionaries make up values within 
+# the base dictionaries and lists as the size grows. This is a quadratic space complexity
--- practiceGraphs.py
+++ practiceGraphs.py
@@ -1,4 +1,6 @@
 ## same file as 'graphs.py' but with print statements to help understand intervals of code; 'graphs.py' is the clean copy
+# *** Routes algorithm not fully correct yet; tends to yield errors on routes with > 3 steps. ***
+# ** Needs recursive functions/quality check to weed out bugs. Will implemented at a later date. ** 
 
 import heapq
 
@@ -29,7 +31,7 @@ class Graph:
 
         while pq:
             print('while begin')
-            route = []
+            print('**pq start:', pq)
             current_distance, current_vertex = heapq.heappop(pq)
             if current_distance > distances[current_vertex]:
                 print('Current Distance:', current_distance)
@@ -37,37 +39,39 @@ class Graph:
                 continue
             print('Vertex:', current_vertex)
             print(self.vertices[current_vertex])
-            for neighbor, weight in self.vertices[current_vertex].items():       
-                print('Weight:', weight)
+            for neighbor, weight in self.vertices[current_vertex].items():  
+                route = []    
+                print('\n-> Neighbor:', neighbor)
+                print('\nWeight:', weight)
                 print('Current Distance:', current_distance)
                 distance = current_distance + weight
                 print(f'distance: {distance} | distances[neighbor]: {distances[neighbor]}')
                 if distance < distances[neighbor]:
-                    print('Neighbor:', neighbor)
+                    
                     print('Vertex:', current_vertex)
                     print(self.vertices[current_vertex])
-                    
-                    step = {current_vertex: current_distance}
-                    print('step1:', step)
-                    if start not in step:
-                        route.append(step)
-                    step = {neighbor: weight}
-                    print('step2:', step)
-                    if start not in step:
-                        route.append(step)
+                    if start != current_vertex:
+                        step = {current_vertex: current_distance}
+
+                        print('step1:', step)
+                        if start not in step:
+                            route.append(step)
+                        step = {neighbor: weight}
+                        print('step2:', step)
+                        if start not in step:
+                            route.append(step)
+                    else:
+                        step = {neighbor: weight}
+                        print('step2:', step)
+                        if start not in step:
+                            route.append(step)
                     paths[neighbor] = route
                     print('paths:', paths)
                     distances[neighbor] = distance
                     print('distances:', distances)
                     heapq.heappush(pq, (distance, neighbor))
-            
-        for item in paths:
-            print()
-            print(item)
-            print(self.vertices[item])
-            if start in self.vertices[item]:
-                print('--->', paths[item])
-                paths[item] = [{item: self.vertices[item][start]}]
+        print(paths)
+        paths = dict(sorted(paths.items()))
         print(f'Paths from {start}:', paths)
         return distances, paths
 
@@ -78,32 +82,65 @@ graph.add_vertex('B')
 graph.add_vertex('C')
 graph.add_vertex('D')
 graph.add_vertex('E')
+graph.add_vertex('F')
+graph.add_vertex('G')
 
 graph.add_edge('A', 'B', 7)
 graph.add_edge('B', 'C', 2)
 graph.add_edge('A', 'C', 8)
-graph.add_edge('A', 'D', 12)
+graph.add_edge('F', 'D', 12)
 graph.add_edge('B', 'E', 10)
 graph.add_edge('D', 'E', 4)
-
-start = 'E'
+graph.add_edge('F', 'B', 2)
+graph.add_edge('G', 'C', 5)
+# Vertices
+# {'A': {'B': 7, 'C': 8}, 
+# 'B': {'A': 7, 'C': 2, 'E': 10, 'F': 2}, 
+# 'C': {'B': 2, 'A': 8, 'G': 5}, 
+# 'D': {'F': 12, 'E': 4}, 
+# 'E': {'B': 10, 'D': 4}, 
+# 'F': {'D': 12, 'B': 2}, 
+# 'G': {'C': 5}}
+start = 'G'
 end = 'A'
+print('Vertices:', graph.vertices)
 distances, paths = graph.dijkstra(start)
 print('\nMinimum distances between points:', distances)
-print(f'Paths between start point {start}: {paths}')
+def pretty_paths(paths):
+    print('\nPaths:')
+    for path in paths:
+        if len(paths[path]) > 1:
+            i = 0
+            for step in paths[path]:
+                if path not in step:
+                    step = paths[list(step.keys())[0]]
+                    paths[path][i] = step
+                    del paths[path][i]
+                    for item in step:
+                        j=0
+                        paths[path].insert(j, item)
+                        j+=1
+                    i+=1
+        print(f'{path} - {paths[path]}')
+    print(paths)
+pretty_paths(paths)
 def display_route(start, end, paths):
     if end not in paths:
         print('\nThe end point is not within the paths database. Please try again!')
     else:
         i=1
-        print(f'\nStart: {start}.')
+        print('\n-*-*-Route-*-*-')
+        print(f'\nStart: {start}. End: {end}')
+        total = 0
         edge0 = start
         for item in paths[end]:
             for point, distance in item.items():
                 edge1 = point
                 print(f'{i}. Take {edge0}{edge1} - {distance} units.')
+                total += distance
                 edge0 = point
                 i+=1
         print(f'{i}. Arrive at {end}.')
+        print(f'Total distance: {total} units.')
         print('Thanks for using Routes!\n')
 display_route(start, end, paths)
